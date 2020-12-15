736000a1abee79e8f3848d8370c7d60d5f859951
1TTO <manupl2002@gmail.com>
2020-12-15 17:43:32 +0100

    Update readMe and change declaration of netmask

--- readMe.md
+++ readMe.md
@@ -1,1 +1,24 @@
-https://github.com/1TTO/SNMP-Scanner 
+# SNMP Scanner
+https://github.com/1TTO/SNMP-Scanner
+
+![User Interface](UI.png)
+
+## Installing:
+Open your project in Intellij and run the command: maven package
+Make sure that in the folder of the artifact you put a folder named data with a mib.csv and oid.csv where
+you insert your mibs and oids.
+Execute your artifact in the command prompt with java -jar (Artifactname).
+Make sure to use java 8 and having included it in the path-variables.
+
+## Functionalities:
+- Scan network with netmask
+- Scan network range
+- Scan single address
+- Customize MIBs
+- Customize OIDs
+- Change Public/Private
+- Change Get/GetNext
+
+## ToDo:
+- Traps/Informs
+- Finish UI
--- src/main/java/manuel/Network.java
+++ src/main/java/manuel/Network.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 
 public class Network {
     private String networkID = "";
-    private int subnetmask;
+    private int netmask;
     private ArrayList<String> networkHosts = new ArrayList<>();
 
-    Network(String singleNetworkAddress, int subnetmask){
-        this.subnetmask = subnetmask;
+    Network(String singleNetworkAddress, int netmask){
+        this.netmask = netmask;
 
-        setNetworkID(singleNetworkAddress, subnetmask);
-        setNetworkHosts(this.networkID, this.subnetmask, 0);
+        setNetworkID(singleNetworkAddress, netmask);
+        setNetworkHosts(this.networkID, this.netmask, 0);
     }
 
     Network(String firstAddress, String lastAddress){
         setNetworkHosts(getFullAddress(firstAddress), getFullAddress(lastAddress), 0);
     }
 
-    private void setNetworkID(String singleNetworkAddress, int subnetmask){
+    private void setNetworkID(String singleNetworkAddress, int netmask){
         String[] networkParts = singleNetworkAddress.split("\\.");
 
         for (int i = 0; i < networkParts.length; i++){
             networkParts[i] = String.format("%03d", Integer.valueOf(networkParts[i]));
         }
 
-        for (int i = 0; i < subnetmask / 8; i++){
+        for (int i = 0; i < netmask / 8; i++){
             if (i == 0) networkID = networkID.concat(networkParts[i]);
             else networkID = networkID.concat(".".concat(networkParts[i]));
         }
 
-        for (int i = subnetmask; i < 32; i += (8 - i % 8)){
+        for (int i = netmask; i < 32; i += (8 - i % 8)){
             int delta = 256 - (int)(Math.pow(2, 32 - i)/Math.pow(256, (3 - i / 8)));
 
             if (i < 8) networkID = networkID.concat(String.format("%03d",delta));
@@ -74,14 +74,14 @@ public class Network {
         }
     }
 
-    private void setNetworkHosts(String networkID, int subnetmask, int recI){
+    private void setNetworkHosts(String networkID, int netmask, int recI){
         String[] networkParts = networkID.split("\\.");
 
-        if (subnetmask < (8 * (recI + 1))){
+        if (netmask < (8 * (recI + 1))){
             if (8 * (recI + 1) != 32){
                 for (int i = Integer.parseInt(networkParts[recI]); i < 256; i++){
                     networkParts[recI] = String.format("%03d", i);
-                    setNetworkHosts(String.join(".", networkParts[0], networkParts[1], networkParts[2], networkParts[3]), subnetmask, recI + 1);
+                    setNetworkHosts(String.join(".", networkParts[0], networkParts[1], networkParts[2], networkParts[3]), netmask, recI + 1);
                     networkParts[recI + 1] = "000";
                 }
             }else{
@@ -89,7 +89,7 @@ public class Network {
                     this.networkHosts.add(String.join(".",networkParts[0], networkParts[1], networkParts[2], String.format("%03d", i)));
                 }
             }
-        }else setNetworkHosts(networkID, subnetmask, recI + 1);
+        }else setNetworkHosts(networkID, netmask, recI + 1);
 
         if (recI == 0){
             this.networkHosts.remove(0);
